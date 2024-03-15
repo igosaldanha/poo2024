0b32a550d93e0311ca8f030d6ba5368c55253643
Igo Saldanha <igosaldanha@alu.ufc.br>
2024-03-15 11:45:42 -0300

    Reoganização de pastas

---
+++ notas_aulas/s03e02_relacionamentos/relacionamentos.ts
@@ -0,0 +1,13 @@
+// Composição
+// Sala e suas paredes
+// sanduiche e o pão
+// nota e sues itens
+
+// Agregação: Tempo de vida interligaod e possibilidade e existir sem o outro, substituição.
+// Lapiseira e Grafite
+// Pessoa e endereço
+
+class Folha {
+    texto: string;
+    
+}
