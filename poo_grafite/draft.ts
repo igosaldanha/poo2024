7de5dd6c969f8240208698dbc3e78418da975cec
Igo Saldanha <igosaldanha@alu.ufc.br>
2024-03-21 20:40:50 -0300

    Organização de pastas

--- poo_grafite/draft.ts
+++ poo_grafite/draft.ts
@@ -2,10 +2,14 @@ class Lead {
     private thickness: number;
     private hardness: string;
     private size: number;
-    
-    
+
     public constructor(thickness: number, hardness: string, size: number) {
+
+        this.thickness = thickness;
+        this.hardness = hardness;
+        this.size = size;
     }
+
     public usagePerSheet(): number {
         if (this.hardness === 'HB')
             return 1;
@@ -18,23 +22,28 @@ class Lead {
         return 0;
     }
     public toString(): string {
+        return `calibre: ${}, grafite: ${}`;
     }
 
     public getThickness(): number {
+        return this.thickness;
     }
     public getSize(): number {
+        return this.size;
     }
     public getHardness(): string {
+        return this.hardness;
     }
     public setSize(value: number): void {
+        this.size = value;
     }
     public setHardness(value: string): void {
+        this.hardness = value;
     }
     public setThickness(value: number): void {
+        this.thickness = value;
     }
 }
-
-
 
 class Pencil {
     private thickness: number;
